1eee5907fcd9f2efcd272d5944248c9e9a9550b0
Eugeniusz Lewandowski <EugeneLewandowski@proton.me>
2025-02-05 13:31:46 +0000

    Fixed file not appending and safety features, have fun

--- main.hs
+++ main.hs
@@ -1,11 +1,13 @@
 import Control.Monad.RWS.Class (MonadState (put))
 import Data.ByteString (StrictByteString)
+import Data.Char (toLower)
 import Distribution.Compat.Lens (use)
 import GHC.IO.FD (openFile)
 import GHC.OldList (find)
+import System.Posix (fileExist)
 import Text.Printf (errorBadArgument)
 import Text.Read (Lexeme (String))
-import Text.XHtml (password)
+import Text.XHtml (content, password)
 
 collapseListExceptLastOp :: [String] -> (String -> String -> String) -> [String]
 collapseListExceptLastOp [] op = error "List can't be empty"
@@ -33,38 +35,58 @@ charParser x "" list = list
 makeCharParser :: Char -> (String -> [String])
 makeCharParser x y = charParser (x ==) y []
 
+loadFile :: String -> IO String
+loadFile filename = do
+  fileExists <- fileExist filename
+  if fileExists
+    then do
+      readFile filename
+    else
+      error "No such file"
+
+stripEmptyLists :: [[a]] -> [[a]]
+stripEmptyLists [] = []
+stripEmptyLists (x : xs)
+  | null x = stripEmptyLists xs
+  | otherwise = x : stripEmptyLists xs
+
+getStoredUserData :: String -> IO [[String]]
+getStoredUserData filename = do
+  contents <- loadFile filename
+  let lineList = stripEmptyLists (lines contents)
+
+  let users = map (makeCharParser ':') lineList
+  let usernameToPassword = map (\users -> collapseListExceptLastOp users (\y x -> y ++ (':' : x))) users
+  return usernameToPassword
+
 handleLogin :: IO ()
 handleLogin =
   do
+    usernameToPassword <- getStoredUserData "./data.txt"
     putStrLn "Enter username"
     username <- getLine
-    contents <- readFile "./userData.txt"
-    let lineList = lines contents
-    let users = map (makeCharParser ':') lineList
-    let usernameToPassword = map (\users -> collapseListExceptLastOp users (\y x -> y ++ (':' : x))) users
 
     putStrLn "Enter password"
     password <- getLine
-    if [username, password] `elem` usernameToPassword
+    if [map toLower username, password] `elem` usernameToPassword
       then
         putStrLn "Successfully signed in"
-      else putStrLn "Unsuccessful login"
+      else do
+        putStrLn "Unsuccessful login"
+        print usernameToPassword
 
 getUsername :: IO String
 getUsername = do
-  contents <- readFile "./userData.txt"
-  let lineList = lines contents
-  let users = map (makeCharParser ':') lineList
-  let usernameToPassword = map (\users -> collapseListExceptLastOp users (\y x -> y ++ (':' : x))) users
-  let usernames = map head usernameToPassword
+  userData <- getStoredUserData "./data.txt"
+  let usernames = map head userData
   putStrLn "Enter username"
   username <- getLine
-  if username `elem` usernames
+  if map toLower username `elem` usernames
     then do
       putStrLn "User already exists try again"
       getUsername
     else
-      return username -- Return the valid password
+      return (map toLower username) -- Return the valid username
 
 getPassword :: IO String
 getPassword = do
@@ -82,7 +104,8 @@ handleSignUp =
   do
     username <- getUsername
     password <- getPassword
-    writeFile "./userData.txt" (username ++ ":" ++ password)
+    appendFile "./data.txt" ("\n" ++ username ++ ":" ++ password)
+    putStrLn (username ++ ":" ++ password)
 
 main :: IO ()
 main = do
